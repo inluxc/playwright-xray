2c1f331ca21b6ec18661523a05e8b0379308546a
Fúlvio Carvalhido <inluxc@gmail.com>
2024-11-20 23:59:43 +0000

    Update with Biome validations

--- src/types/xray.types.ts
+++ src/types/xray.types.ts
@@ -1,34 +1,34 @@
-import { AxiosProxyConfig } from 'axios';
+import type { AxiosProxyConfig } from "axios";
 
 export interface XrayOptions {
-  jira: {
-    url: string;
-    type: string;
-    apiVersion: string;
-  };
-  cloud?: {
-    client_id?: string;
-    client_secret?: string;
-    xrayUrl?: string;
-  };
-  server?: {
-    token: string;
-  };
-  projectKey: string;
-  testPlan: string;
-  testExecution?: string;
-  revision?: string;
-  description?: string;
-  testEnvironments?: string[];
-  version?: string;
-  debug: boolean;
-  proxy?: AxiosProxyConfig;
-  uploadScreenShot?: boolean;
-  uploadTrace?: boolean;
-  uploadVideo?: boolean;
-  markFlakyWith?: string;
-  stepCategories: string[];
-  summary: string;
-  dryRun?: boolean;
-  runResult?: boolean;
+	jira: {
+		url: string;
+		type: string;
+		apiVersion: string;
+	};
+	cloud?: {
+		client_id?: string;
+		client_secret?: string;
+		xrayUrl?: string;
+	};
+	server?: {
+		token: string;
+	};
+	projectKey: string;
+	testPlan: string;
+	testExecution?: string;
+	revision?: string;
+	description?: string;
+	testEnvironments?: string[];
+	version?: string;
+	debug: boolean;
+	proxy?: AxiosProxyConfig;
+	uploadScreenShot?: boolean;
+	uploadTrace?: boolean;
+	uploadVideo?: boolean;
+	markFlakyWith?: string;
+	stepCategories: string[];
+	summary: string;
+	dryRun?: boolean;
+	runResult?: boolean;
 }
--- tests/simple.test.ts
+++ tests/simple.test.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('XRAYISSUE-1 | basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+test("XRAYISSUE-1 | basic test", async ({ page }) => {
+	await page.goto("https://playwright.dev/");
+	const title = page.locator(".navbar__inner .navbar__title");
+	await expect(title).toHaveText("Playwright");
 });
