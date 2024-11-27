cdbbbf81ea6d0317d53d4893554ed3f7525a406f
Fúlvio Carvalhido <inluxc@gmail.com>
2024-11-27 03:01:44 +0000

    Fix with biome

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import * as fs from 'node:fs';
-import * as path from 'node:path';
-import type { FullConfig, Reporter, Suite, TestCase, TestResult, TestStep } from '@playwright/test/reporter';
-import { blue, bold, green, red, white, yellow } from 'picocolors';
-import type { XrayTest, XrayTestEvidence, XrayTestResult, XrayTestSteps } from './types/cloud.types';
-import type { XrayOptions } from './types/xray.types';
+import * as fs from "node:fs";
+import * as path from "node:path";
+import type { FullConfig, Reporter, Suite, TestCase, TestResult, TestStep } from "@playwright/test/reporter";
+import { blue, bold, green, red, white, yellow } from "picocolors";
+import type { XrayTest, XrayTestEvidence, XrayTestResult, XrayTestSteps } from "./types/cloud.types";
+import type { XrayOptions } from "./types/xray.types";
 
-import Help from './help';
-import type { ExecInfo } from './types/execInfo.types';
-import { XrayService } from './xray.service';
+import Help from "./help";
+import type { ExecInfo } from "./types/execInfo.types";
+import { XrayService } from "./xray.service";
 
 class XrayReporter implements Reporter {
   private xrayService!: XrayService;
@@ -22,7 +22,7 @@ class XrayReporter implements Reporter {
   private uploadScreenShot: boolean | undefined;
   private uploadTrace: boolean | undefined;
   private uploadVideo: boolean | undefined;
-  private stepCategories = ['expect', 'pw:api', 'test.step'];
+  private stepCategories = ["expect", "pw:api", "test.step"];
 
   constructor(options: XrayOptions) {
     this.options = options;
@@ -49,45 +49,45 @@ class XrayReporter implements Reporter {
       tests: [] as XrayTest[],
     };
     this.testResults = testResults;
-    console.log(`${bold(blue('-------------------------------------'))}`);
-    console.log(`${bold(blue(' '))}`);
+    console.log(`${bold(blue("-------------------------------------"))}`);
+    console.log(`${bold(blue(" "))}`);
     if (this.options.summary !== undefined) testResults.info.summary = this.options.summary;
     this.execInfo = {
-      browserName: '',
+      browserName: "",
     };
   }
 
   onBegin(config: FullConfig, suite: Suite) {
     config.projects.forEach((p, index) => {
-      this.execInfo.browserName += index > 0 ? ', ' : '';
+      this.execInfo.browserName += index > 0 ? ", " : "";
       this.execInfo.browserName += p.name.charAt(0).toUpperCase() + p.name.slice(1);
     });
     if (this.options.dryRun) {
-      console.log(`${bold(yellow('⏺  '))}${bold(blue(`Starting a Dry Run with ${suite.allTests().length} tests`))}`);
+      console.log(`${bold(yellow("⏺  "))}${bold(blue(`Starting a Dry Run with ${suite.allTests().length} tests`))}`);
     } else {
-      console.log(`${bold(yellow('⏺  '))}${bold(blue(`Starting the run with ${suite.allTests().length} tests`))}`);
+      console.log(`${bold(yellow("⏺  "))}${bold(blue(`Starting the run with ${suite.allTests().length} tests`))}`);
     }
 
-    console.log(`${bold(blue(' '))}`);
+    console.log(`${bold(blue(" "))}`);
   }
 
   async onTestBegin(test: TestCase) {
     if (this.execInfo.testedBrowser === undefined) {
       this.execInfo.testedBrowser = test.parent.parent?.title;
       console.log(
-        `${bold(yellow('⏺  '))}${bold(blue(`The following test execution will be imported & reported:  ${this.execInfo.testedBrowser}`))}`,
+        `${bold(yellow("⏺  "))}${bold(blue(`The following test execution will be imported & reported:  ${this.execInfo.testedBrowser}`))}`,
       );
     }
   }
   async onTestEnd(testCase: TestCase, result: TestResult) {
     const testCaseId = testCase.title.match(this.testCaseKeyPattern);
-    const testCode: string = testCaseId?.[1] ?? '';
+    const testCode: string = testCaseId?.[1] ?? "";
     const projectId = JSON.stringify(testCase.parent.project()).match(/__projectId":"(.*)"/)?.[1];
     if (this.execInfo.testedBrowser !== projectId) {
       return;
     }
 
-    if (testCode !== '') {
+    if (testCode !== "") {
       // @ts-ignore
       const finishTime = new Date(result.startTime.getTime() + result.duration);
       this.totalDuration = this.totalDuration + result.duration;
@@ -98,11 +98,11 @@ class XrayReporter implements Reporter {
         start: this.help.getFormatData(result.startTime),
         finish: this.help.getFormatData(finishTime),
         steps: [] as XrayTestSteps[],
-        comment: '',
+        comment: "",
       };
 
       // Set Test Error
-      const pwStepsExists = result.steps.some((step) => step.category.includes('test.step'));
+      const pwStepsExists = result.steps.some((step) => step.category.includes("test.step"));
       if (result.errors.length > 0 && !pwStepsExists) {
         xrayTestData.comment = JSON.stringify(result.errors);
       } else {
@@ -112,15 +112,15 @@ class XrayReporter implements Reporter {
               // Add Step to request
               const errorMessage = this.stripAnsi(step);
               const received = errorMessage ? this.receivedRegEx.exec(errorMessage) : null;
-              let dataReceived = '';
+              let dataReceived = "";
               if (received?.[1] !== undefined) {
                 dataReceived = received?.[1];
               }
 
               const xrayTestStep: XrayTestSteps = {
                 status:
-                  typeof step.error === 'object' ? this.help.convertPwStatusToXray('failed') : this.help.convertPwStatusToXray('passed'),
-                comment: typeof step.error === 'object' ? errorMessage : '',
+                  typeof step.error === "object" ? this.help.convertPwStatusToXray("failed") : this.help.convertPwStatusToXray("passed"),
+                comment: typeof step.error === "object" ? errorMessage : "",
                 actualResult: dataReceived,
               };
               xrayTestData.steps?.push(xrayTestStep);
@@ -133,13 +133,13 @@ class XrayReporter implements Reporter {
       const evidences: XrayTestEvidence[] = [];
       if (result.attachments.length > 0) {
         result.attachments.map(async (attach) => {
-          if (attach.name.includes('screenshot') && this.uploadScreenShot) {
+          if (attach.name.includes("screenshot") && this.uploadScreenShot) {
             await this.addEvidence(attach, evidences);
           }
-          if (attach.name.includes('trace') && this.uploadTrace) {
+          if (attach.name.includes("trace") && this.uploadTrace) {
             await this.addEvidence(attach, evidences);
           }
-          if (attach.name.includes('video') && this.uploadVideo) {
+          if (attach.name.includes("video") && this.uploadVideo) {
             await this.addEvidence(attach, evidences);
           }
         });
@@ -147,23 +147,23 @@ class XrayReporter implements Reporter {
 
       xrayTestData.evidence = evidences;
       this.testResults.tests?.push(xrayTestData);
-      let projectID = '';
+      let projectID = "";
       const tst: string = JSON.stringify(testCase.parent.project()).match(/__projectIdd":"(.*)"/)?.[1] as string;
       if (tst !== undefined) {
         projectID = `${tst.charAt(0).toUpperCase() + tst.slice(1)} | `;
       }
 
       switch (this.help.convertPwStatusToXray(result.status)) {
-        case 'PASS':
-        case 'PASSED':
+        case "PASS":
+        case "PASSED":
           console.log(`${bold(green(`✅ ${projectID}${testCase.title}`))}`);
           break;
-        case 'FAIL':
-        case 'FAILED':
+        case "FAIL":
+        case "FAILED":
           console.log(`${bold(red(`⛔ ${projectID}${testCase.title}`))}`);
           break;
-        case 'SKIPPED':
-        case 'ABORTED':
+        case "SKIPPED":
+        case "ABORTED":
           console.log(`${bold(white(`🚫 ${projectID}${testCase.title}`))}`);
           break;
       }
@@ -171,13 +171,13 @@ class XrayReporter implements Reporter {
   }
 
   private stripAnsi(step: TestStep) {
-    const ST = '(?:\\u0007|\\u001B\\u005C|\\u009C)';
+    const ST = "(?:\\u0007|\\u001B\\u005C|\\u009C)";
     const pattern = [
       `[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?${ST})`,
-      '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))',
-    ].join('|');
+      "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))",
+    ].join("|");
     let errorMessage = step.error?.stack?.valueOf();
-    errorMessage = errorMessage?.replace(new RegExp(pattern, 'g'), '');
+    errorMessage = errorMessage?.replace(new RegExp(pattern, "g"), "");
     return errorMessage;
   }
 
@@ -191,10 +191,10 @@ class XrayReporter implements Reporter {
     evidences: XrayTestEvidence[],
   ) {
     if (!attach.path) {
-      throw new Error('Attachment path is undefined');
+      throw new Error("Attachment path is undefined");
     }
     const filename = path.basename(attach.path);
-    const attachData = fs.readFileSync(attach.path, { encoding: 'base64' });
+    const attachData = fs.readFileSync(attach.path, { encoding: "base64" });
     const evid: XrayTestEvidence = {
       data: attachData,
       filename: filename,
@@ -209,7 +209,7 @@ class XrayReporter implements Reporter {
       new Date(new Date(this.testResults?.info?.startDate ?? new Date()).getTime() + this.totalDuration),
     );
 
-    if (typeof this.testResults !== 'undefined' && typeof this.testResults.tests !== 'undefined' && this.testResults.tests.length > 0) {
+    if (typeof this.testResults !== "undefined" && typeof this.testResults.tests !== "undefined" && this.testResults.tests.length > 0) {
       await this.xrayService.createRun(this.testResults, this.execInfo);
     } else {
       console.log(`There are no tests with such ${this.testCaseKeyPattern} key pattern`);
@@ -218,4 +218,4 @@ class XrayReporter implements Reporter {
 }
 
 export default XrayReporter;
-export * from './types/xray.types';
+export * from "./types/xray.types";
