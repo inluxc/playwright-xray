c4577d1e717024587792ec918af93516f0a0e4ac
Niklas-Back <148854658+Niklas-Back@users.noreply.github.com>
2025-08-06 17:09:16 +0200

    Upgraded Biome to 2.1.3

--- tests/test-with-retries.test.ts
+++ tests/test-with-retries.test.ts
@@ -1,13 +1,13 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
 test.describe(() => {
   test.describe.configure({ retries: 3 });
-  test('test fails three times', async ({ page }, testinfo) => {
-    await page.goto('https://example.org');
+  test("test fails three times", async ({ page }, testinfo) => {
+    await page.goto("https://example.org");
     if (testinfo.retry < 3) {
-      await expect(page.getByRole('heading')).toHaveText('Hello');
+      await expect(page.getByRole("heading")).toHaveText("Hello");
     } else {
-      await expect(page.getByRole('heading')).toHaveText('Example Domain');
+      await expect(page.getByRole("heading")).toHaveText("Example Domain");
     }
   });
 });
